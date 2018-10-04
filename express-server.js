8fc9f8b438b7118ba72935dc38b6abcc720a58dc
Jim <yjzlin@gmail.com>
2018-10-04 21:46:10 +0000

    Finished user specific features, bug checking not done yet

--- express-server.js
+++ express-server.js
@@ -4,25 +4,21 @@ var app = express()
 var PORT = 8080
 const cookieParser = require('cookie-parser')
 
-// Generate random 6 string function
-function generateRandomString() {
-  var alphanumeric = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
-  var result = ''
-
-  for (var i = 0; i < 6; i++) {
-    result += alphanumeric[Math.floor(Math.random() * alphanumeric.length)]
-  }
-
-  return result
-}
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 app.set('view engine', 'ejs')
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const urlDatabase = {
+  'b2xvn2': {
+    userID: "k98mep",
+    shortURL: 'b2xvn2',
+    longURL: "http://www.lighthouselabs.ca"
+  },
+  '9sm5xk': {
+    userID: "b4525",
+    shortURL: '9sm5xK',
+    longURL: "http://www.google.com"
+  }
 }
 
 const users = {
@@ -38,16 +34,68 @@ const users = {
   }
 }
 
+// Generate random 6 string function
+function generateRandomString() {
+  var alphanumeric = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
+  var result = ''
+
+  for (var i = 0; i < 6; i++) {
+    result += alphanumeric[Math.floor(Math.random() * alphanumeric.length)]
+  }
+
+  return result
+}
+
+// Function to filter out urls for individual users
+const urlsForUsers = (id) => {
+  let object = {}
+
+  for (let link in urlDatabase) {
+    if (urlDatabase[link].userID === id) {
+      object[link] = urlDatabase[link]
+    }
+  }
+
+  return object
+}
+
+// Function to match given email with email from users database
+
+const match = (email) => {
+  let matching;
+
+  for (let user in users) {
+    if (users[user].email === email) {
+      return matching = users[user]
+    } else {
+      matching = false
+    }
+  }
+  return matching
+}
+
 app.get('/', (req, res) => {
   res.redirect('/urls')
 })
 
 
 app.get('/urls', (req, res) => {
-  let templateVars = {
-    user_id: users[req.cookies.user_id],
-    urls: urlDatabase
+
+  let templateVars;
+
+  if (req.cookies.user_id) {
+    const filteredLinks = urlsForUsers(req.cookies.user_id)
+      templateVars = {
+      user_id: users[req.cookies.user_id],
+      urls: urlDatabase,
+      sorted: filteredLinks
+    }
+  } else {
+    templateVars = {
+      user_id: users[req.cookies.user_id],
+    }
   }
+
   res.render('urls_index', templateVars)
 })
 
@@ -61,11 +109,21 @@ app.get('/urls/new', (req, res) => {
 
 app.get('/urls/:id', (req, res) => {
   let templateVars = { user_id: users[req.cookies.user_id], shortURL: req.params.id, urls: urlDatabase }
-  res.render('urls_show', templateVars)
+
+  if (!req.cookies.user_id) {
+    res.send('Please login first!')
+  } else if (!urlDatabase[req.params.id]) {
+    res.send('Does not exist!')
+  } else if (urlDatabase[req.params.id].userID !== req.cookies.user_id) {
+    res.send('No permission to view this page!')
+  } else {
+   res.render('urls_show', templateVars)
+  }
 })
 
 app.get('/u/:shortURL', (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL]
+
+  let longURL = urlDatabase[req.params.shortURL].longURL
   res.redirect(longURL)
 })
 
@@ -76,13 +134,23 @@ app.post('/urls', (req, res) => {
 })
 
 app.post('/urls/:id', (req, res) => {
-  urlDatabase[req.params.id] = 'http://' + req.body.longURLNew
-  res.redirect('/urls')
+  const linkOwner = urlDatabase[req.params.id].userID
+  if (req.cookies.user_id !== linkOwner) {
+    res.send('Sorry, no permission to do that')
+  } else {
+    urlDatabase[req.params.id].longURL = 'http://' + req.body.longURLNew
+    res.redirect('/urls')
+  }
 })
 
 app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id]
-  res.redirect('/urls')
+  const linkOwner = urlDatabase[req.params.id].userID
+  if (req.cookies.user_id !== linkOwner) {
+    res.send('Sorry, no permission to do that')
+  } else {
+    delete urlDatabase[req.params.id]
+    res.redirect('/urls')
+  }
 })
 
 app.get('/login', (req, res) => {
@@ -92,21 +160,7 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
   const { email, password } = req.body
 
-  const match = (email) => {
-    let matching;
-
-    for (let user in users) {
-      if (users[user].email === email) {
-        return matching = users[user]
-      } else {
-        matching = false
-      }
-    }
-    return matching
-  }
-
   const userMatch = match(email)
-  console.log(userMatch)
 
   if (userMatch && userMatch.password === password) {
     res.cookie('user_id', userMatch.id)
