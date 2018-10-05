48affc3ed490acc79d4fe2e308eeddf30bace6e6
Jim <yjzlin@gmail.com>
2018-10-05 02:29:50 +0000

    Hopefully replaced all with ES6 syntax

--- express-server.js
+++ express-server.js
@@ -40,10 +40,10 @@ const users = {
 
 // Generate random 6 string function
 const generateRandomString = () => {
-  var alphanumeric = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
-  var result = ''
+  const alphanumeric = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
+  let result = ''
 
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     result += alphanumeric[Math.floor(Math.random() * alphanumeric.length)]
   }
 
